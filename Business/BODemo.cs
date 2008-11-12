409f84680e3063b51b786274502aa22ee63d28e2
vinsonhwj <vinsonhwj@9c1399e2-9aa0-11dd-bbfe-3dabce05a288>
2008-11-12 06:56:29 +0000

    git-svn-id: https://vinsondataaccess.googlecode.com/svn/trunk@25 9c1399e2-9aa0-11dd-bbfe-3dabce05a288

---
+++ Business/BODemo.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using WongTung.DataAccess;
+using WongTung.Entity.Table;
+using hwj.DBUtility;
+namespace WongTung.Business
+{
+    public class BODemo
+    {
+        private static DATestTable da = new DATestTable();
+        public static string GetSelect()
+        {
+            SelectFields sf = new SelectFields();
+            sf.Add(testtable.Fields.Name);
+            sf.Add(testtable.Fields.Phone);
+            
+            WhereParam wp = new WhereParam();
+            wp.AddParam(testtable.Fields.CreateOn, DateTime.Now.AddDays(-1), Enums.Operator.Greater);
+
+            OrderFields op = new OrderFields();
+            op.AddParam(testtable.Fields.CreateOn, Enums.OrderBy.Descending);
+
+            da.GetList(sf, wp, op, 1);
+            return da.SqlCommand;
+        }
+        public static List<Entity.Table.testtable> GetAll()
+        {
+            return da.GetSpecialEntity();
+        }
+        public static Entity.Table.testtable GetSingle()
+        {
+            return da.GetEntity();
+        }
+        public static DataTable GetDataTable()
+        {
+            return da.GetDataTable();
+        }
+        public static string GetCount()
+        {
+            return da.RecordCount().ToString() + "\r\n" + da.SqlCommand;
+        }
+        public static string Add()
+        {
+            testtable t = new testtable();
+            t.Name = "vinson";
+            t.Phone = 123;
+            t.CreateOn = DateTime.Now;
+            da.Add(t);
+            return da.SqlCommand;
+        }
+        public static string Update()
+        {
+            UpdateParam up = new UpdateParam();
+            up.AddParam(testtable.Fields.CreateOn, DateTime.Now.AddYears(-1));
+            WhereParam wp = new WhereParam();
+            wp.AddParam(testtable.Fields.Name, "vinson", Enums.Operator.Equal);
+
+            da.Update(up);
+            return da.SqlCommand;
+        }
+        public static string Delete()
+        {
+            WhereParam wp = new WhereParam();
+            wp.AddParam(testtable.Fields.Phone, "1234", Enums.Operator.Equal);
+
+            da.Delete(wp);
+            return da.SqlCommand;
+        }
+
+    }
+}
