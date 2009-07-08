7fe1742575fe720665719481e9d2a41237014d45
vinsonhwj <vinsonhwj@9c1399e2-9aa0-11dd-bbfe-3dabce05a288>
2009-07-08 10:26:49 +0000

    git-svn-id: https://vinsondataaccess.googlecode.com/svn/trunk@104 9c1399e2-9aa0-11dd-bbfe-3dabce05a288

--- Test/Suggest.Designer.cs
+++ Test/Suggest.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.suggestBox1 = new hwj.UserControls.Suggest.SuggestBox();
+            this.xTextBox1 = new hwj.UserControls.CommonControls.xTextBox();
             this.SuspendLayout();
             // 
             // suggestBox1
@@ -47,20 +48,33 @@
             this.suggestBox1.TabIndex = 0;
             this.suggestBox1.DataBinding += new System.EventHandler(this.suggestBox1_DataBinding);
             // 
+            // xTextBox1
+            // 
+            this.xTextBox1.ShowContentError = true;
+            this.xTextBox1.ContentType = hwj.UserControls.CommonControls.ContentType.Numberic;
+            this.xTextBox1.Format = null;
+            this.xTextBox1.Location = new System.Drawing.Point(397, 214);
+            this.xTextBox1.Name = "xTextBox1";
+            this.xTextBox1.Size = new System.Drawing.Size(100, 21);
+            this.xTextBox1.TabIndex = 1;
+            // 
             // Suggest
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(612, 412);
+            this.Controls.Add(this.xTextBox1);
             this.Controls.Add(this.suggestBox1);
             this.Name = "Suggest";
             this.Text = "Suggest";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private hwj.UserControls.Suggest.SuggestBox suggestBox1;
+        private hwj.UserControls.CommonControls.xTextBox xTextBox1;
     }
 }
