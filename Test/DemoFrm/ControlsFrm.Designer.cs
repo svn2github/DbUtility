486200ec1f7c2ca68db5bc35b014b493039f413b
vinsonhwj <vinsonhwj@9c1399e2-9aa0-11dd-bbfe-3dabce05a288>
2010-04-26 17:11:20 +0000

    git-svn-id: https://vinsondataaccess.googlecode.com/svn/trunk@161 9c1399e2-9aa0-11dd-bbfe-3dabce05a288

--- Test/DemoFrm/ControlsFrm.Designer.cs
+++ Test/DemoFrm/ControlsFrm.Designer.cs
@@ -29,12 +29,45 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ControlsFrm));
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.xDateTimePicker3 = new hwj.UserControls.CommonControls.xDateTimePicker();
+            this.xDateTimePicker2 = new hwj.UserControls.CommonControls.xDateTimePicker();
+            this.xDateTimePicker1 = new hwj.UserControls.CommonControls.xDateTimePicker();
             this.xTextBox1 = new hwj.UserControls.CommonControls.xTextBox();
             this.txtTestAmtTxt = new hwj.UserControls.CommonControls.xButton();
             this.txtAmt = new hwj.UserControls.CommonControls.xTextBox();
             this.btnAddText = new hwj.UserControls.CommonControls.xButton();
             this.loginComboBox1 = new hwj.UserControls.Other.LoginComboBox();
+            this.maskedDateTimePicker1 = new hwj.UserControls.Other.MaskedDateTimePicker();
             this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            resources.ApplyResources(this.monthCalendar1, "monthCalendar1");
+            this.monthCalendar1.Name = "monthCalendar1";
+            // 
+            // xDateTimePicker3
+            // 
+            this.xDateTimePicker3.InputFormat = null;
+            resources.ApplyResources(this.xDateTimePicker3, "xDateTimePicker3");
+            this.xDateTimePicker3.Name = "xDateTimePicker3";
+            this.xDateTimePicker3.SetValueToControl = null;
+            // 
+            // xDateTimePicker2
+            // 
+            resources.ApplyResources(this.xDateTimePicker2, "xDateTimePicker2");
+            this.xDateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.xDateTimePicker2.InputFormat = null;
+            this.xDateTimePicker2.Name = "xDateTimePicker2";
+            this.xDateTimePicker2.SetValueToControl = null;
+            // 
+            // xDateTimePicker1
+            // 
+            resources.ApplyResources(this.xDateTimePicker1, "xDateTimePicker1");
+            this.xDateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.xDateTimePicker1.InputFormat = "";
+            this.xDateTimePicker1.Name = "xDateTimePicker1";
+            this.xDateTimePicker1.SetValueToControl = null;
             // 
             // xTextBox1
             // 
@@ -80,17 +113,35 @@
             this.loginComboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
             this.loginComboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
             this.loginComboBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.loginComboBox1.ConfigName = null;
             this.loginComboBox1.DefaultDisplayLastRecord = true;
             this.loginComboBox1.FileName = "";
             this.loginComboBox1.FormattingEnabled = true;
             resources.ApplyResources(this.loginComboBox1, "loginComboBox1");
             this.loginComboBox1.Name = "loginComboBox1";
             this.loginComboBox1.OldBackColor = System.Drawing.SystemColors.Window;
+            this.loginComboBox1.UserConfigSetting = null;
+            // 
+            // maskedDateTimePicker1
+            // 
+            this.maskedDateTimePicker1.BackColor = System.Drawing.SystemColors.Window;
+            this.maskedDateTimePicker1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.maskedDateTimePicker1.Format = "yyyy/M/d";
+            resources.ApplyResources(this.maskedDateTimePicker1, "maskedDateTimePicker1");
+            this.maskedDateTimePicker1.MinimumSize = new System.Drawing.Size(50, 21);
+            this.maskedDateTimePicker1.Name = "maskedDateTimePicker1";
+            this.maskedDateTimePicker1.ShowCheckBox = false;
+            this.maskedDateTimePicker1.Value = new System.DateTime(2010, 4, 27, 0, 32, 1, 210);
             // 
             // ControlsFrm
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.maskedDateTimePicker1);
+            this.Controls.Add(this.xDateTimePicker3);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.xDateTimePicker2);
+            this.Controls.Add(this.xDateTimePicker1);
             this.Controls.Add(this.xTextBox1);
             this.Controls.Add(this.txtTestAmtTxt);
             this.Controls.Add(this.txtAmt);
@@ -109,5 +160,10 @@
         private hwj.UserControls.CommonControls.xTextBox txtAmt;
         private hwj.UserControls.CommonControls.xButton txtTestAmtTxt;
         private hwj.UserControls.CommonControls.xTextBox xTextBox1;
+        private hwj.UserControls.CommonControls.xDateTimePicker xDateTimePicker1;
+        private hwj.UserControls.CommonControls.xDateTimePicker xDateTimePicker2;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private hwj.UserControls.CommonControls.xDateTimePicker xDateTimePicker3;
+        private hwj.UserControls.Other.MaskedDateTimePicker maskedDateTimePicker1;
     }
 }
