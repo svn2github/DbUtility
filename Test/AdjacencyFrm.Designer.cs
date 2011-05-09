15ca089ad32e9cf78f2859caf3990487514afbaf
vinsonhwj@gmail.com <vinsonhwj@gmail.com@9c1399e2-9aa0-11dd-bbfe-3dabce05a288>
2011-05-09 10:46:31 +0000

    git-svn-id: https://vinsondataaccess.googlecode.com/svn/trunk@191 9c1399e2-9aa0-11dd-bbfe-3dabce05a288

---
+++ Test/AdjacencyFrm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Test
+{
+    partial class AdjacencyFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.xButton1 = new hwj.UserControls.CommonControls.xButton();
+            this.SuspendLayout();
+            // 
+            // xButton1
+            // 
+            this.xButton1.CursorFromClick = System.Windows.Forms.Cursors.WaitCursor;
+            this.xButton1.Location = new System.Drawing.Point(205, 26);
+            this.xButton1.Name = "xButton1";
+            this.xButton1.Size = new System.Drawing.Size(75, 23);
+            this.xButton1.TabIndex = 0;
+            this.xButton1.Text = "xButton1";
+            this.xButton1.UseVisualStyleBackColor = true;
+            this.xButton1.Click += new System.EventHandler(this.xButton1_Click);
+            // 
+            // AdjacencyFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 266);
+            this.Controls.Add(this.xButton1);
+            this.Name = "AdjacencyFrm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AdjacencyFrm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private hwj.UserControls.CommonControls.xButton xButton1;
+    }
+}
---
+++ Test/AdjacencyFrm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class AdjacencyFrm : Form
+    {
+        public AdjacencyFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void xButton1_Click(object sender, EventArgs e)
+        {
+            AdjacencyList<string> a = new AdjacencyList<string>();
+            //a.AddVertex("V1");
+            //a.AddVertex("V2");
+            //a.AddVertex("V3");
+            //a.AddVertex("V4");
+            //a.AddVertex("V5");
+            //a.AddVertex("V6");
+            //a.AddVertex("V7");
+            //a.AddVertex("V8");
+            //a.AddEdge("V1", "V2");
+            //a.AddEdge("V1", "V3");
+            //a.AddEdge("V2", "V4");
+            //a.AddEdge("V2", "V5");
+            //a.AddEdge("V3", "V6");
+            //a.AddEdge("V3", "V7");
+            //a.AddEdge("V4", "V8");
+            //a.AddEdge("V5", "V8");
+            //a.AddEdge("V6", "V8");
+            //a.AddEdge("V7", "V8");
+
+            a.AddVertex("A");
+            a.AddVertex("B");
+            a.AddVertex("C");
+            a.AddVertex("D");
+            a.AddEdge("A", "B");
+            a.AddEdge("A", "D");
+            a.AddEdge("B", "C");
+            a.AddEdge("C", "D");
+
+            a.Search2("A", "C");
+            a.DFSTraverse();
+            a.BFSTraverse();
+        }
+    }
+}
